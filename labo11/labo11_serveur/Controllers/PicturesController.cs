aa460639070e48dd93bf4d21c5bbee925d7ba594
Danik <2290726@cegepmontpetit.ca>
2024-11-13 14:12:28 -0500

    Ajout fichiers labo 11

---
+++ labo11/labo11_serveur/Controllers/PicturesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using semaine11_serveur.Data;
+using semaine11_serveur.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace semaine11_serveur.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        private readonly semaine11_serveurContext _context;
+
+        public PicturesController(semaine11_serveurContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetPictureIds()
+        {
+            // À modifier
+            return Ok();
+        }
+
+        [HttpGet("{size}/{id}")]
+        public async Task<ActionResult> GetPicture(string size, int id)
+        {
+            // À modifier
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostPicture()
+        {
+            // À modifier
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePicture(int id)
+        {
+            // À modifier
+            return Ok();
+        }
+    }
+}
---
+++ labo11/labo11_serveur/Models/Picture.cs
@@ -0,0 +1,7 @@
+﻿namespace semaine11_serveur.Models
+{
+    public class Picture
+    {
+        // salut ça va ? ta session se passe bien ? tu prends soin de toi ? 
+    }
+}
